df5b4582e76ec466a25c70e74f995fc2aa9b3d6d
Noman <gmabdullahalnoman@gmail.com>
2025-07-28 16:10:19 +0600

    trying diff

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class BasicProgram {
-	public static void public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello Github");
+	}	
+
+		static void second {
+		System.out.println("working on git diff and commit");
 	}
 }
