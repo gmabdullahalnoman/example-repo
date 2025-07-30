1bf56f99e83fde2b8f74bbaea58d2b39974776cb
Noman <gmabdullahalnoman@gmail.com>
2025-07-30 22:16:27 +0600

    added XOR solution by comment

--- MissingNumber.java
+++ MissingNumber.java
@@ -15,3 +15,22 @@ public class MissingNumber {
         System.out.println(expectedSum - actualSum);
     }
 }
+/*
+public class MissingNumberXOR {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        long expectedXORSum = 0;
+        for (int i=1;i<=n;i++){
+            expectedXORSum ^= i;
+        }
+        long XORinput = 0;
+        for (int i =0 ; i < n-1 ; i++){
+            XORinput ^= input.nextLong();
+        }
+        int missing = (int) (expectedXORSum ^ XORinput);
+        System.out.println(missing);
+    }
+}
+*/
