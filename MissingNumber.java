3131236e8576cd495f909c8e169e6b7778407b65
Noman <gmabdullahalnoman@gmail.com>
2025-07-30 21:50:02 +0600

    Little error in logic,solved and updated.

--- MissingNumber.java
+++ MissingNumber.java
@@ -6,10 +6,10 @@ public class MissingNumber {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long expectedSum = n* (n-1) /2;
+        long expectedSum = (long) n* (n+1) /2;
 
         long actualSum = 0;
-        for (int i =1 ; i<n-1; i++){
+        for (int i =0 ; i<n-1; i++){
             actualSum += sc.nextLong();
         }
         System.out.println(expectedSum - actualSum);
